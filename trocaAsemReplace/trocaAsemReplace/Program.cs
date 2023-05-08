747e071cca7e2c3b77d44c8248c3455c9c4f1b98
Rodrigo Spicker Reis Silva <r.spicker13@gmail.com>
2023-05-08 17:14:45 -0300

    trocaAsemReplace

---
+++ trocaAsemReplace/trocaAsemReplace/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trocaAsemReplace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            trocaAsemReplace();
+        }
+
+        static void trocaAsemReplace()
+        {
+
+            Console.Write("Digite uma frase: ");
+            string frase = Console.ReadLine();
+            char[] chars = frase.ToCharArray();
+            Console.Write("Frase digitada trocando 'A' por '&' fica: ");
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] == 'A')
+                {
+                    chars[i] = '&';
+                }
+                Console.Write(chars[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
