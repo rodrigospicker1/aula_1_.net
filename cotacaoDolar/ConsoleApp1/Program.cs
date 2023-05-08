d1d5654dc03ef051779544014810a395348a1b17
Rodrigo Spicker Reis Silva <r.spicker13@gmail.com>
2023-05-08 16:37:18 -0300

    cotacaoDolar

---
+++ cotacaoDolar/ConsoleApp1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            cotacaoDolar();
+        }
+
+        static void cotacaoDolar()
+        {
+
+            double cotacao = 0.0;
+            double valor = 0.0;
+            double valorConvertido = 0.0;
+
+            do
+            {
+                Console.WriteLine("Digite a cotação do dolar");
+                Console.Write("Cotação: ");
+            }
+            while (!double.TryParse(Console.ReadLine().Replace(".",","), out cotacao));
+            do
+            {
+                Console.WriteLine("Digite o valor que você deseja converter");
+                Console.Write("$: ");
+            }
+            while (!double.TryParse(Console.ReadLine().Replace(".", ","), out valor));
+            valorConvertido = (float)(valor * cotacao);
+            Console.Write("O valor digitado em dólar pra reais é, R$ ");
+            Console.Write(valorConvertido.ToString("0.00"));
+            Console.ReadKey();
+
+        }
+
+    }
+}
