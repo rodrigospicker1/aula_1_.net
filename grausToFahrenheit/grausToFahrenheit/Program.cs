6e73588ad29a525b63f50415579d617e05737d0f
Rodrigo Spicker Reis Silva <r.spicker13@gmail.com>
2023-05-08 16:36:00 -0300

    grausToFahrenheit

---
+++ grausToFahrenheit/grausToFahrenheit/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grausToFahrenheit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            grausToFahrenheit();
+        }
+
+        static void grausToFahrenheit()
+        {
+            double tempCelsius = 0.0;
+            double tempFahrenheit = 0.0;
+
+            do
+            {
+                Console.Write("Digite a temperatura em graus celsius: ");
+            }
+            while (!double.TryParse(Console.ReadLine().Replace(".", ","), out tempCelsius));
+            tempFahrenheit = (9 * tempCelsius + 160) / 5;
+            Console.Write("A temperatura {0}° Celsius em Fahrenheit fica {1}", tempCelsius, tempFahrenheit);
+            Console.ReadKey();
+
+        }
+    }
+}
