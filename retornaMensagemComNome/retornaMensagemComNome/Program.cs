363c4ba6f8b6d150c3f043dd6ab04cd5231bb704
Rodrigo Spicker Reis Silva <r.spicker13@gmail.com>
2023-05-08 17:36:49 -0300

    retornaMensagemComNome

---
+++ retornaMensagemComNome/retornaMensagemComNome/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace retornaMensagemComNome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            retornaNomeComMensagem();
+        }
+
+        static void retornaNomeComMensagem()
+        {
+            Console.Write("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Olá meu nome é: {0}", nome);
+            Console.ReadKey();
+        }
+    }
+}
